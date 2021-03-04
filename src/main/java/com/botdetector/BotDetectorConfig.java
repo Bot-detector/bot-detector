d21513b837af70d50f8ead3ab602e4cd129d424f
Ferrariic <thearcticfalcon@gmail.com>
2021-03-04 03:42:11 -0500

    Create BotDetectorConfig.java

---
+++ src/main/java/com/botdetector/BotDetectorConfig.java
@@ -0,0 +1,23 @@
+package net.runelite.client.plugins.botdetector;
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("botdetector")
+public interface BotDetectorConfig extends Config
+{
+    @ConfigItem(
+            position = 1,
+            keyName = "sendAutomatic",
+            name = "Send names automatically",
+            description = "Toggle automatic name sending"
+    )
+    default boolean sendAutomatic() { return false; }
+    @ConfigItem(
+            position = 2,
+            keyName = "intConfig",
+            name = "Send every 'X' minutes:",
+            description = "Minimum value: Sent every 5 minutes."
+    )
+    default int intConfig() { return 5; }
+}
