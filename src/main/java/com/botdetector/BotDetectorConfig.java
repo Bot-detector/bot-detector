a1505cd8bb39d728c2799a4452fb0020d669ad47
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2021-03-04 17:32:52 -0500

    Added a check to prevent duplicates from being added to the player list.

--- src/main/java/com/botdetector/BotDetectorConfig.java
+++ src/main/java/com/botdetector/BotDetectorConfig.java
@@ -1,4 +1,4 @@
-package com.botdetector;
+package net.runelite.client.plugins.botdetector;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
