7df5ad974893eba4b4d338c8b0e3d6b8b18f1cd7
Cyborger1 <45152844+Cyborger1@users.noreply.github.com>
2021-04-27 13:22:07 -0400

    Make "Delete" option string static

--- src/main/java/com/botdetector/BotDetectorPlugin.java
+++ src/main/java/com/botdetector/BotDetectorPlugin.java
@@ -61,8 +61,9 @@ public class BotDetectorPlugin extends Plugin
 {
 	private static final String DETECT = "Detect";
 	private static final String KICK_OPTION = "Kick";
+	private static final String DELETE_OPTION = "Delete";
 	private static final ImmutableSet<String> AFTER_OPTIONS =
-		ImmutableSet.of("Message", "Add ignore", "Remove friend", "Delete", KICK_OPTION);
+		ImmutableSet.of("Message", "Add ignore", "Remove friend", DELETE_OPTION, KICK_OPTION);
 
 	private static final char CODE_COMMAND_INDICATOR = '!';
 	private static final String CODE_COMMAND = "code";
@@ -463,7 +464,7 @@ public class BotDetectorPlugin extends Plugin
 			groupId == WidgetInfo.RAIDING_PARTY.getGroupId() || groupId == WidgetInfo.PRIVATE_CHAT_MESSAGE.getGroupId() ||
 			groupId == WidgetInfo.IGNORE_LIST.getGroupId())
 		{
-			if (!AFTER_OPTIONS.contains(option) || (option.equals("Delete") && groupId != WidgetInfo.IGNORE_LIST.getGroupId()))
+			if (!AFTER_OPTIONS.contains(option) || (option.equals(DELETE_OPTION) && groupId != WidgetInfo.IGNORE_LIST.getGroupId()))
 			{
 				return;
 			}
