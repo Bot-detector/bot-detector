8f8994499b0de4c14a515444b9195abad3df4939
Cyborger1 <45152844+Cyborger1@users.noreply.github.com>
2021-06-05 20:07:51 -0400

    Update prediction feedback tooltip text

--- src/main/java/com/botdetector/ui/BotDetectorPanel.java
+++ src/main/java/com/botdetector/ui/BotDetectorPanel.java
@@ -681,8 +681,8 @@ public class BotDetectorPanel extends PluginPanel
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
 
-		String tooltip = "<html>Please tell us if this prediction seems %s to you!" +
-			"<br>Doing so will help us improve our model.</html>";
+		String tooltip = "<html>Tell us if the <b>primary prediction</b> seems %s to you! Doing so will help us improve our model." +
+			"<br><span style='color:red'>Please</span>, do not vote against a prediction simply because the percentage is not high enough.</html>";
 
 		feedbackHeaderLabel = new JLabel("Is this prediction correct?");
 		feedbackHeaderLabel.setHorizontalTextPosition(JLabel.LEFT);
