d2b3d7cd4406c5dc7dda105232ee8d575f4f14be
Cyborger1 <45152844+Cyborger1@users.noreply.github.com>
2024-03-05 16:53:07 -0500

    Only check if prediction confidence isn't null

--- src/main/java/com/botdetector/http/BotDetectorClient.java
+++ src/main/java/com/botdetector/http/BotDetectorClient.java
@@ -400,15 +400,15 @@ public class BotDetectorClient
 				{
 					Prediction p = processResponse(gson, response, Prediction.class);
 					// Sanity check for if the primary label does not appear in the breakdown
-					if (p != null && p.getPredictionBreakdown() != null && !p.getPredictionBreakdown().isEmpty())
+					if (p != null
+						&& p.getConfidence() != null // Some 'debug' labels such as 'Stats_Too_Low' will have null confidence, ignore these!
+						&& p.getPredictionBreakdown() != null
+						&& !p.getPredictionBreakdown().isEmpty()
+						&& p.getPredictionBreakdown().keySet().stream().noneMatch(x -> p.getPredictionLabel().equalsIgnoreCase(x)))
 					{
-						if (p.getPredictionBreakdown().keySet().stream()
-							.noneMatch(x -> p.getPredictionLabel().equalsIgnoreCase(x)))
-						{
-							p.getPredictionBreakdown().put(p.getPredictionLabel(), p.getConfidence());
-							log.warn(String.format("Primary prediction label missing from breakdown! Added missing label. (pl:'%s', id:'%d', lb:'%s', cf:'%.4f')",
-								p.getPlayerName(), p.getPlayerId(), p.getPredictionLabel(), p.getConfidence()));
-						}
+						p.getPredictionBreakdown().put(p.getPredictionLabel(), p.getConfidence());
+						log.warn(String.format("Primary prediction label missing from breakdown! Added missing label. (pl:'%s', id:'%d', lb:'%s', cf:'%.4f')",
+							p.getPlayerName(), p.getPlayerId(), p.getPredictionLabel(), p.getConfidence()));
 					}
 					future.complete(p);
 				}
